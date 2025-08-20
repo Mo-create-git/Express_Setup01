3045b410a86a70b91434e8e15c316d824ef62049
Mo-create-git <msaffan078@gmail.com>
2025-08-20 16:00:48 +0530

    Fully working Backend

--- server/app.js
+++ server/app.js
@@ -2,21 +2,25 @@ import express from "express";
 
 const app = express();
 const PORT = 5002;
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello world");
-});
-
-app.get("/Register", (req,res) => {
-    res.send("This is a registration page");
 });
 
 app.get("/Global", (req,res) => {
     res.send("Welcome To Global Collage");
 }); 
 
+app.post("/register", (req, res) => {
+    const userData = req.body; // { name, email, password, ... }
+  
+    console.log("📥 Incoming /register payload:", userData);
+  
+  });
+
 app.listen( PORT, () => {
-    console.log("Server is running on http://localhost:${PORT}");
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 
